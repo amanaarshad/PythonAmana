5888d2a9a1c0947d70cdaaacd81255afcc9fcd22
amanaarshad <41306680+amanaarshad@users.noreply.github.com>
2024-01-18 20:17:54 -0800

    this is an example of how set works in the python

---
+++ set.py
@@ -0,0 +1,9 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Hello world")
+myset = set()
+myset.add(2)
+print(myset)
+#--------------------------------------------------
+myset = {1,2,3}
+print(myset)
